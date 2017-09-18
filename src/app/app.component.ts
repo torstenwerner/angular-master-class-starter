d124260117cf3ae72488923cc12b4f2e33f23c17
Torsten Werner <torsten.werner@westernacher.com>
2017-09-18 11:24:28 +0200

    exercise 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -8,19 +9,5 @@ import { Contact } from './models/contact';
 })
 export class ContactsAppComponent {
   title = 'Angular Master Class setup works!';
-  contact: Contact = {
-    id: 6,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  }
+  contacts: Contact[] = CONTACT_DATA;
 }
