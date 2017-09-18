68b282451914e6d5eb64010bba843ef266cc1ec6
Torsten Werner <torsten.werner@westernacher.com>
2017-09-18 11:29:16 +0200

    add MdListModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MdToolbarModule } from '@angular/material';
+import { MdToolbarModule, MdListModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ContactsAppComponent } from './app.component';
@@ -11,7 +11,7 @@ import { ContactsAppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule,
+    MdToolbarModule, MdListModule,
     FlexLayoutModule
   ],
   bootstrap: [ContactsAppComponent]
