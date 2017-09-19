f08a77bc4f2fa8686e90df6200bbfbf41e939477
Torsten Werner <torsten.werner@westernacher.com>
2017-09-19 09:18:43 +0200

    exercise 9

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -9,12 +10,12 @@ import { ContactsService } from '../contacts.service';
 })
 export class ContactsListComponent implements OnInit {
 
-  contacts: Contact[]
+  contacts$: Observable<Array<Contact>>;
 
   constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
-    this.contactsService.getContacts().subscribe(contacts => this.contacts = contacts);
+    this.contacts$ = this.contactsService.getContacts();
   }
 
   trackByContactId(index, contact) {
