29f1237d6c41f84adba75c29e345268be1cb0b9f
Torsten Werner <torsten.werner@westernacher.com>
2017-09-20 10:43:24 +0200

    type safety

--- src/app/store/root-reducer.ts
+++ src/app/store/root-reducer.ts
@@ -2,7 +2,7 @@ import { Store } from '@ngrx/store';
 import { voteReducer } from './votes/vote-reducer';
 import { VotesState } from './votes/vote-state';
 import { createStore, combineReducers } from 'redux';
-import { InjectionToken } from '@angular/core';
+import { FactoryProvider, InjectionToken } from '@angular/core';
 
 const ROOT_REDUCER = {
   votes: voteReducer
@@ -18,5 +18,5 @@ export interface ApplicationState {
 
 export const APP_STORE = new InjectionToken<Store<ApplicationState>>('appStore');
 
-export const APP_STORE_PROVIDER = { provide: APP_STORE, useFactory: appStoreFactory };
+export const APP_STORE_PROVIDER: FactoryProvider = { provide: APP_STORE, useFactory: appStoreFactory };
 
